8e3d2a845938064197170018d7ac46729dd1f5b9
Your GitHub Username <Your GitHub Email>
2026-07-23 22:50:49 +0700

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,7 @@ function figmaAssetResolver() {
 
 export default defineConfig({
   base: "/MyPortofolio/",
-  plugins: [
-    figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [figmaAssetResolver(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
